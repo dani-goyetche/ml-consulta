f4003e81ed7a4e1ca0c7e2968e5ec0ca76d38d91
dani-goyetche <goyetche.daniela@gmail.com>
2021-08-29 22:59:15 -0300

    Logo en header

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get (
-    '/',
-    (req, res) => res.send("Hola mundo!")
-);
+app.use(express.static('public')),
+
+//app.get ('/', (req, res) => res.send("Hola mundo!"));
+app.get('/', (req, res) => res.sendFile(path.resolve("views/home.html")));
 
 app.listen(3000, () => {
     console.log('App listening on port http://localhost:3000/');
